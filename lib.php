3b02a700316fa817f18c551625c1ee09ff5485cd
Giulia Alfonsi <electric.g@gmail.com>
2023-02-03 14:50:58 -0800

    Fixed localhost cors

--- lib.php
+++ lib.php
@@ -6,7 +6,7 @@ function get($json) {
     $request_headers        = apache_request_headers();
     $http_origin            = $request_headers['Origin'];
     $allowed_http_origins   = array(
-        "http://localhost",
+        "http://localhost:8080",
     );
     if (in_array($http_origin, $allowed_http_origins)){  
         @header("Access-Control-Allow-Origin: " . $http_origin);
