fc2c14aac5b472b8bb37c366f0c9876daf2ea518
Giulia Alfonsi <electric.g@gmail.com>
2023-02-03 15:43:45 -0800

    More cleaning

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-
 header('Content-Type: application/json; charset=utf-8');
 
+$remote_url = 'https://vancouver-gbfs.smoove.pro/gbfs/2/en/';
+$folder = '/mobi-api/';
 $allowed_jsons = array(
     'gbfs_versions.json',
     'system_information.json',
@@ -10,20 +11,14 @@ $allowed_jsons = array(
     'vehicle_types.json',
     'free_bike_status.json',
 );
-
-$folder = '/mobi-api/';
-
-$remote_url = 'https://vancouver-gbfs.smoove.pro/gbfs/2/en/';
-
-$allowed_http_origins   = array(
+$allowed_http_origins = array(
     'http://localhost:8080',
     'https://giulia.dev',
     'https://giugee.com/',
     'https://electricg.github.io',
 );
 
-$requested = $_SERVER['REQUEST_URI'];
-$parsed_requested = str_replace($folder, '', $requested);
+$parsed_requested = str_replace($folder, '', $_SERVER['REQUEST_URI']);
 
 if (!in_array($parsed_requested, $allowed_jsons)) {
     http_response_code(404);
@@ -31,15 +26,12 @@ if (!in_array($parsed_requested, $allowed_jsons)) {
     exit();
 }
 
-$request_headers        = apache_request_headers();
-$http_origin            = $request_headers['Origin'];
+$request_headers = apache_request_headers();
+$http_origin = $request_headers['Origin'];
 
-if (in_array($http_origin, $allowed_http_origins)) {  
+if (in_array($http_origin, $allowed_http_origins)) {
     @header('Access-Control-Allow-Origin: ' . $http_origin);
 }
-// header('Access-Control-Allow-Origin: *');
 
-$url = $remote_url . $parsed_requested;
-echo file_get_contents($url);
-
+echo file_get_contents($remote_url . $parsed_requested);
 ?>
