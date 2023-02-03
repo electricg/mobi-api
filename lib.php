76d4ba03a8e5468350f6144482d0f7f66a7bdc20
Giulia Alfonsi <electric.g@gmail.com>
2023-02-03 14:05:08 -0800

    Open cors

--- lib.php
+++ lib.php
@@ -4,6 +4,8 @@ function get($json) {
 
     header('Content-Type: application/json; charset=utf-8');
 
+    header('Access-Control-Allow-Origin: *');
+
     $url = "https://vancouver-gbfs.smoove.pro/gbfs/2/en/" . $json;
 
     echo file_get_contents($url);
