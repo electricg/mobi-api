dbccc8b0b51942e1c27967e2b1be9b8b6d63e59d
Giulia Alfonsi <electric.g@gmail.com>
2023-02-03 13:33:08 -0800

    Get station_status.json

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 
 // Show all information, defaults to INFO_ALL
-phpinfo();
+// phpinfo();
+
+$url = "https://vancouver-gbfs.smoove.pro/gbfs/2/en/station_status.json";
+
+echo file_get_contents($url);
 
 ?>
