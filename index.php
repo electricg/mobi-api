688d145264b1c4c43781f3e54029d8ed8d11e471
Giulia Alfonsi <electric.g@gmail.com>
2024-01-31 16:33:13 -0800

    Better code to check the validity of the request

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
 
-$remote_url = 'https://vancouver-gbfs.smoove.pro/gbfs/2/en/';
-$allowed_jsons = array(
+$REMOTE_URL = 'https://vancouver-gbfs.smoove.pro/gbfs/2/en/';
+$ALLOWED_JSONS = array(
     'gbfs_versions.json',
     'system_information.json',
     'station_information.json',
@@ -10,27 +10,32 @@ $allowed_jsons = array(
     'vehicle_types.json',
     'free_bike_status.json',
 );
-$local_folder = '/mobi-api/';
-$allowed_http_origins = array(
+$ALLOWED_HTTP_ORIGINS = array(
     'https://giulia.dev',
     'https://giugee.com',
     'https://electricg.github.io',
 );
+$IS_LOCALHOST_ALLOWED = true;
 
 $request_headers = apache_request_headers();
 $http_origin = $request_headers['Origin'];
 
-if (in_array($http_origin, $allowed_http_origins) || preg_match("/^http:\/\/localhost(:\d+)?$/", $http_origin)) {
+if (
+        in_array($http_origin, $ALLOWED_HTTP_ORIGINS) ||
+        ($IS_LOCALHOST_ALLOWED && preg_match("/^http:\/\/localhost(:\d+)?$/", $http_origin))
+    ) {
     header('Access-Control-Allow-Origin: ' . $http_origin);
 }
 
-$parsed_requested = str_replace($local_folder, '', $_SERVER['REQUEST_URI']);
+$uri_parts = explode('?', $_SERVER['REQUEST_URI']);
+$uri_folders = explode('/', $uri_parts[0]);
+$parsed_requested = array_pop($uri_folders);
 
-if (!in_array($parsed_requested, $allowed_jsons)) {
+if (!in_array($parsed_requested, $ALLOWED_JSONS)) {
     http_response_code(404);
     echo '{"error":"Not Found"}';
     exit();
 }
 
-echo file_get_contents($remote_url . $parsed_requested);
+echo file_get_contents($REMOTE_URL . $parsed_requested);
 ?>
